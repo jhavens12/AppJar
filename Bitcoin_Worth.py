2cc741dd18312510c441beea6b1e9516971ffded
jhavens <jhavens12@gmail.com>
2017-12-05 10:38:33 -0500

    Updated with Worth

--- Bitcoin_Worth.py
+++ Bitcoin_Worth.py
@@ -4,31 +4,60 @@ from exchanges.coindesk import CoinDesk
 from time import sleep
 from datetime import datetime
 
+invested = 122.44
+owned = .01
+
 time = datetime.now()
 timestamp = str(time.time().strftime('%I:%M %p'))
 
-
-price = '${:,.2f}'.format(CoinDesk.get_current_price(currency='USD'))
+price = CoinDesk.get_current_price(currency='USD')
+price_USD = '${:,.2f}'.format(price)
+worth = float(price) * owned
+worth_USD = '${:,.2f}'.format(float(price) * owned)
+margin = worth - invested
+margin_USD = '${:,.2f}'.format(margin)
 
 def update_label():
     time = datetime.now()
     timestamp = str(time.time().strftime('%I:%M %p'))
-    price = '${:,.2f}'.format(CoinDesk.get_current_price(currency='USD'))
+    price = CoinDesk.get_current_price(currency='USD')
+    price_USD = '${:,.2f}'.format(price)
+    worth = float(price) * owned
+    worth_USD = '${:,.2f}'.format(float(price) * owned)
+    margin = worth - invested
+    margin_USD = '${:,.2f}'.format(margin)
+
     app.clearLabel('L1')
-    app.setLabel('L1',price)
+    app.setLabel('L1',price_USD)
+
     app.clearLabel('L2')
-    app.setLabel('L2',timestamp)
+    app.setLabel('L2',margin_USD)
+    if margin > 0:
+        app.setLabelFg('L2', 'dark green')
+    else:
+        app.setLabelFg('L2', 'red')
+
+    app.clearLabel('L3')
+    app.setLabel('L3',timestamp)
 
 app = gui('Test1' '480x320')
 app.setGeometry('fullscreen')
 
 app.setPollTime(30000)
-app.setFont(72)
-app.addLabel('L1', price, 1,1,3)
-app.addLabel('L2', timestamp, 2,1,3)
+app.setFont(68)
+app.addLabel('L1', price_USD, 1,1,3)
+app.addLabel('L2', margin_USD, 2,1,3)
+app.addLabel('L3', timestamp, 3,1,3)
 app.setLabelFg('L1', 'blue')
-app.setLabelFg('L2', 'red')
-app.getLabelWidget("L2").config(font="Courier 20")
+
+if worth > 0:
+    app.setLabelFg('L2', 'dark green')
+else:
+    app.setLabelFg('L2', 'red')
+
+app.setLabelFg('L3', 'red')
+app.getLabelWidget("L2").config(font="Verdana 54")
+app.getLabelWidget("L3").config(font="Courier 20")
 app.registerEvent(update_label)
 
 app.go()
